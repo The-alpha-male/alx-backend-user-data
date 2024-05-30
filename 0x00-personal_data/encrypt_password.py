a93cc1f17aced40a2720a5b9485caee10118ba8e
The-alpha-male <ronnymunene@outlook.com>
2024-05-31 00:36:38 +0300

    function that expects 2 arguments and returns a boolean

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,16 +1,29 @@
 #!/usr/bin/env python3
-"""Defines a hash password function to return a hashed password"""
+"""
+Defines a hash_password function to return a hashed password
+"""
 import bcrypt
 from bcrypt import hashpw
 
 
-def hash_password(password, str) -> bytes:
-    """Returns a hash password"""
+def hash_password(password: str) -> bytes:
+    """
+    Returns a hashed password
+    Args:
+        password (str): password to be hashed
+    """
     b = password.encode()
     hashed = hashpw(b, bcrypt.gensalt())
     return hashed
 
 
 def is_valid(hashed_password: bytes, password: str) -> bool:
-    """Check if the password is valid"""
-    return bcrypt.is_valid(password.encode(), hashed_password)
+    """
+    Check whether a password is valid
+    Args:
+        hashed_password (bytes): hashed password
+        password (str): password in string
+    Return:
+        bool
+    """
+    return bcrypt.checkpw(password.encode(), hashed_password)
