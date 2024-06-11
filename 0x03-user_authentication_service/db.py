4af57fafce1b919d69c692655cd2c6a5946d99cd
The-alpha-male <ronnymunene@outlook.com>
2024-06-11 19:42:13 +0300

    implemented the find_user_by method that uses kwargs

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
-"""DB module
 """
-from flask import Flask, jsonify, request, abort, Response
+DB module
+"""
 from sqlalchemy import create_engine
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-from sqlalchemy.exc import InvalidRequestError
-from sqlalchemy.orm.exc import NoResultFound
+
 from user import Base, User
 
 
@@ -18,7 +19,7 @@ class DB:
     def __init__(self) -> None:
         """Initialize a new DB instance
         """
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
@@ -33,7 +34,7 @@ class DB:
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """adds a user to the database
+        """Add a user to the database
         """
         user = User(email=email, hashed_password=hashed_password)
         self._session.add(user)
@@ -41,7 +42,8 @@ class DB:
         return user
 
     def find_user_by(self, **kwargs) -> User:
-        """Finds a user by keyword arguments"""
+        """Find a user by keyword arguments
+        """
         if not kwargs:
             raise InvalidRequestError()
         user = self._session.query(User).filter_by(**kwargs).first()
