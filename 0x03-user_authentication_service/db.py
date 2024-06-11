a206b82227d90482845f90b9f68baf7da609d302
The-alpha-male <ronnymunene@outlook.com>
2024-06-11 19:26:01 +0300

    implemented the find_user_by method that uses kwargs

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -18,7 +18,7 @@ class DB:
     def __init__(self) -> None:
         """Initialize a new DB instance
         """
-        self._engine = create_engine("sqlite:///a.db")
+        self._engine = create_engine("sqlite:///a.db", echo=True)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
@@ -38,4 +38,13 @@ class DB:
         user = User(email=email, hashed_password=hashed_password)
         self._session.add(user)
         self._session.commit()
+        return user
+
+    def find_user_by(self, **kwargs) -> User:
+        """Finds a user by keyword arguments"""
+        if not kwargs:
+            raise InvalidRequestError()
+        user = self._session.query(User).filter_by(**kwargs).first()
+        if user is None:
+            raise NoResultFound()
         return user
