83812985e517389a3f2ba8078f2dada7b345dbf8
The-alpha-male <ronnymunene@outlook.com>
2024-06-12 21:20:11 +0300

    Implemented the end-point to register a user

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -11,6 +11,18 @@ AUTH = Auth()
 def index():
     return jsonify({"message": "Bienvenue"})
 
+@app.route('/users', methods=['POST'])
+def users() -> str:
+    """"Users post method"""
+    email = request.form.get('password')
+    password = request.form.get('password')
+    try:
+        user = AUTH.register_user(email, password)
+        return jsonify({"email": email, "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
+
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
