f87fd00f554ab64a6f4e68ad2879b797370a42a8
The-alpha-male <ronnymunene@outlook.com>
2024-06-13 02:32:04 +0300

    Implemented the Auth.valid_login method.

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -26,3 +26,11 @@ class Auth:
         except NoResultFound:
             return self._db.add_user(email, _hash_password(password))
         raise ValueError('User {} already exists.'.format(email))
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """Check password validity"""
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            return False
+        return bcrypt.checkpw(password.encode('utf-8'), user.hashed_password)
