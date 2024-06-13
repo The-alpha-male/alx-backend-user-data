f706450ce41acb12b936dd688c92ab8f2d653834
The-alpha-male <ronnymunene@outlook.com>
2024-06-13 17:34:28 +0300

    Implemented the Auth.get_user_from_session_id method. It takes a single session_id string argument and returns the corresponding  None

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -49,3 +49,12 @@ class Auth:
             return user.session_id
         except NoResultFound:
             return None
+
+    def get_user_from_session_id(self, session_id: str) -> User:
+        """Find a user by session"""
+        if session_id is None:
+            return None
+        try:
+            return self._db.find_user_by(session_id=session_id)
+        except NoResultFound:
+            return None
