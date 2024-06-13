89bfa604ab45233804da8dd4f1c86959df2b3efe
The-alpha-male <ronnymunene@outlook.com>
2024-06-14 01:19:14 +0300

    End to end integration testing

--- 0x03-user_authentication_service/main.py
+++ 0x03-user_authentication_service/main.py
@@ -8,86 +8,86 @@ from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.orm.exc import NoResultFound
 from auth import _hash_password, Auth
 
-# print(User.__tablename__)
+print(User.__tablename__)
 
-# for column in User.__table__.columns:
-#     print("{}: {}".format(column, column.type))
+for column in User.__table__.columns:
+    print("{}: {}".format(column, column.type))
 
-# my_db = DB()
+my_db = DB()
 
-# user_1 = my_db.add_user("test@test.com", "SuperHashedPwd")
-# print(user_1.id)
+user_1 = my_db.add_user("test@test.com", "SuperHashedPwd")
+print(user_1.id)
 
-# user_2 = my_db.add_user("test1@test.com", "SuperHashedPwd1")
-# print(user_2.id)
-# user = my_db.add_user("test@test.com", "PwdHashed")
-# print(user.id)
+user_2 = my_db.add_user("test1@test.com", "SuperHashedPwd1")
+print(user_2.id)
+user = my_db.add_user("test@test.com", "PwdHashed")
+print(user.id)
 
-# find_user = my_db.find_user_by(email="test@test.com")
-# print(find_user.id)
+find_user = my_db.find_user_by(email="test@test.com")
+print(find_user.id)
 
-# try:
-#     find_user = my_db.find_user_by(email="test2@test.com")
-#     print(find_user.id)
-# except NoResultFound:
-#     print("Not found")
+try:
+    find_user = my_db.find_user_by(email="test2@test.com")
+    print(find_user.id)
+except NoResultFound:
+    print("Not found")
 
-# try:
-#     find_user = my_db.find_user_by(no_email="test@test.com")
-#     print(find_user.id)
-# except InvalidRequestError:
-#     print("Invalid")
-# email = 'test@test.com'
-# hashed_password = "hashedPwd"
+try:
+    find_user = my_db.find_user_by(no_email="test@test.com")
+    print(find_user.id)
+except InvalidRequestError:
+    print("Invalid")
+email = 'test@test.com'
+hashed_password = "hashedPwd"
 
-# user = my_db.add_user(email, hashed_password)
-# print(user.id)
+user = my_db.add_user(email, hashed_password)
+print(user.id)
 
-# try:
-#     my_db.update_user(user.id, hashed_password='NewPwd')
-#     print("Password updated")
-# except ValueError:
-#     print("Error")
+try:
+    my_db.update_user(user.id, hashed_password='NewPwd')
+    print("Password updated")
+except ValueError:
+    print("Error")
 
-# print(_hash_password("Hello Holberton"))
+print(_hash_password("Hello Holberton"))
 
-# email = 'me@me.com'
-# password = 'mySecuredPwd'
+email = 'me@me.com'
+password = 'mySecuredPwd'
 
-# auth = Auth()
+auth = Auth()
 
-# try:
-#     user = auth.register_user(email, password)
-#     print("successfully created a new user!")
-# except ValueError as err:
-#     print("could not create a new user: {}".format(err))
+try:
+    user = auth.register_user(email, password)
+    print("successfully created a new user!")
+except ValueError as err:
+    print("could not create a new user: {}".format(err))
 
-# try:
-#     user = auth.register_user(email, password)
-#     print("successfully created a new user!")
-# except ValueError as err:
-#     print("could not create a new user: {}".format(err))
+try:
+    user = auth.register_user(email, password)
+    print("successfully created a new user!")
+except ValueError as err:
+    print("could not create a new user: {}".format(err))
 
-# email = 'bob@bob.com'
-# password = 'MyPwdOfBob'
-# auth = Auth()
+email = 'bob@bob.com'
+password = 'MyPwdOfBob'
+auth = Auth()
 
-# auth.register_user(email, password)
+auth.register_user(email, password)
 
-# print(auth.valid_login(email, password))
+print(auth.valid_login(email, password))
 
-# print(auth.valid_login(email, "WrongPwd"))
+print(auth.valid_login(email, "WrongPwd"))
 
-# print(auth.valid_login("unknown@email", password))
+print(auth.valid_login("unknown@email", password))
 
-# email = 'bob@bob.com'
-# password = 'MyPwdOfBob'
-# auth = Auth()
+email = 'bob@bob.com'
+password = 'MyPwdOfBob'
+auth = Auth()
 
-# auth.register_user(email, password)
+auth.register_user(email, password)
 
-# print(auth.create_session(email))
-# print(auth.create_session("unknown@email.com"))
+print(auth.create_session(email))
+print(auth.create_session("unknown@email.com"))
 
 EMAIL = "guillaume@holberton.io"
 PASSWD = "b4l0u"
